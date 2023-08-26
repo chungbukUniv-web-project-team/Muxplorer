826fa51b690b0a0442f3839a462044044f3699a4
bobo <qhfka8253@naver.com>
2023-08-26 20:10:30 +0900

    feat : 수정 header, footer 수정

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,11 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import oc from 'open-color';
+import { shadow, media } from './styleUtil';
 
-function Header(){
-    return(
-            <header style={{ backgroundColor: "black", color: "white", textAlign: "center", padding: "1rem" }}>
-                <h1>Muxplore</h1>
-                <p>충북대학교 학식 플랫폼</p>
-            </header>
+// 상단 고정, 그림자
+const Positioner = styled.div`
+    display: flex;
+    flex-direction: column;
+    //position: fixed; >> 이거하면 헤더를 계속 노출시킬 순 있음 대신 모든페이지 상단 여백 필요
+    top: 0px;
+    width: 100%;
+    ${shadow(1)}
+`;
+
+// 흰 배경, 내용 중간 정렬
+const WhiteBackground = styled.div`
+    background: white;
+    display: flex;
+    justify-content: center;
+    height: auto;
+`;
+
+// 해더의 내용
+const HeaderContents = styled.div`
+    width: 1200px;
+    height: 55px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    padding-right: 1rem;
+    padding-left: 1rem;
+    ${media.wide`
+        width: 992px;
+    `}
+
+    ${media.tablet`
+        width: 100%;
+    `}
+`;
+
+// 로고
+const Logo = styled.div`
+    font-size: 1.4rem;
+    letter-spacing: 2px;
+    color: black;//${oc.teal[7]};
+    font-family: 'Rajdhani';
+`;
+
+// 중간 여백
+const Spacer = styled.div`
+    flex-grow: 1;
+`;
+
+// 하단 그래디언트 테두리
+const GradientBorder = styled.div`
+    height: 3px;
+    background: linear-gradient(to right, #444, black);//(to right, ${oc.teal[6]}, ${oc.cyan[5]});
+`;
+
+const Header = ({children}) => {
+    return (
+        <Positioner>
+            <WhiteBackground>
+                <HeaderContents>
+                    <Logo>MUXPLORER</Logo>
+                    <Spacer/>
+                    {children}
+                </HeaderContents>
+            </WhiteBackground>
+            <GradientBorder/>
+        </Positioner>
     );
-}
+};
 
 export default Header;
